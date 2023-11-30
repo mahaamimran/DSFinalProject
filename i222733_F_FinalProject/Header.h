e0b92a41b38a10df67d2094731b38174f4a9a585
mahaamimran <mahaamimran@gmail.com>
2023-11-30 10:26:36 +0500

    car moves in a 5x5 grid

--- i222733_F_FinalProject/Header.h
+++ i222733_F_FinalProject/Header.h
@@ -1,13 +1,5 @@
-//
-//  Header.h
-//  i222733_F_FinalProject
-//
-//  Created by Maham on 27/11/2023.
-//
-
 #ifndef Header_h
 #define Header_h
-
 #include <iostream>
 #include <list>
 #include <queue>
@@ -17,22 +9,9 @@
 #include <ctime>
 
 using namespace std;
-class Car{
-    char symbol;
-public:
-    Car():symbol('x'){}
-    Car(char s):symbol(s){}
-    char getSymbol(){return symbol;}
-    void setSymbol(char s){symbol = s;}
-
-
-
-
-};
 class Graph {
     int V;
-    list<pair<int, int> >*adjList; // for weighted graph
-
+    list<pair<int, int> >*adjList; 
 public:
     Graph():V(0),adjList(nullptr){}
     Graph(int v):V(v){
@@ -42,7 +21,6 @@ public:
     void setV(int v){V = v;}
     list<pair<int, int> >* getAdjList(){return adjList;}
     void setAdjList(list<pair<int, int> >* adjlist){adjList = adjlist;}
-
     void addEdge(int u, int v, int weight=0,bool bidir=true){
         adjList[u].push_back(make_pair(v, weight));
         if(bidir)adjList[v].push_back(make_pair(u, weight));
@@ -63,74 +41,108 @@ public:
         }
         return 0;
     }
-void display(int rows, int columns,int carPlace=0) {
+    void display(int rows, int columns,int carPlace=0) {
     // Each node is represented by a 'x'
     // Each edge is represented by a '-' or '|'
     // Check if edge exists between two nodes
     // If yes, print '-' or '|'
     // Else print ' '
     // Print edge based on weight, e.g., weight 1 = '-', weight 2 = '--', weight 3 = '---' 
-
-    for (int i = 0; i < rows; i++) {
-        for (int j = 0; j < columns; j++) {
-            if(i*columns+j==carPlace)cout<<'C';
-            else cout << "x";
-            if (j != columns - 1) {
-                if (doesEdgeExist(i * columns + j, i * columns + j + 1)) {
-                    for (int k = 0; k < getWeight(i * columns + j, i * columns + j + 1); k++) {
-                        cout << "-";
-                    }
-                } else {
-                    for (int k = 0; k < getWeight(i * columns + j, i * columns + j + 1); k++) {
-                        cout << " ";
-                    }
-                }
-            }
-        }
-        cout << endl;
-        if (i != rows - 1) {
+        for (int i = 0; i < rows; i++) {
             for (int j = 0; j < columns; j++) {
-                if (doesEdgeExist(i * columns + j, (i + 1) * columns + j)) {
-                    //for (int k = 0; k < getWeight(i * columns + j, (i + 1) * columns + j); k++) {
-                        cout << "|";
-                   // }
-                } else {
-                    // for (int k = 0; k < getWeight(i * columns + j, (i + 1) * columns + j); k++) {
-                        //cout << " ";
-                   // }
-                }
+                int u=i*columns+j;
+                int v=i*columns+j+1;
+                if(i*columns+j==carPlace)cout<<"C";
+                else cout << "x";
                 if (j != columns - 1) {
-                     for (int k = 0; k < getWeight(i * columns + j, i * columns + j + 1); k++) {
-                        cout << " ";
+                    if (doesEdgeExist(u, v)) {
+                        for (int k = 0; k < getWeight(i * columns + j, v); k++) {
+                            cout << "-";
+                        }
+                    } else {
+                        for (int k = 0; k < getWeight(i * columns + j, v); k++) {
+                            cout << " ";
+                        }
                     }
                 }
             }
+            cout << endl;
+            if (i != rows - 1) {
+                for (int j = 0; j < columns; j++) {
+                    int u=i*columns+j;
+                    int v=(i+1)*columns+j;
+                    if (doesEdgeExist(u, v)) {
+                        //for (int k = 0; k < getWeight(u, v); k++) {
+                            cout << "|";
+                    // }
+                    } else {
+                        // for (int k = 0; k < getWeight(u, v); k++) {
+                            //cout << " ";
+                    // }
+                    }
+                    if (j != columns - 1) {
+                        for (int k = 0; k < getWeight(u, i * columns + j + 1); k++) {
+                            cout << " ";
+                        }
+                    }
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
     }
-}
     ~Graph() {
         delete[] adjList;
     }
 };
 
 void generateMap(Graph& g) {    
-    // creating a graph with 9 vertices (3*3)
-    // 0 1 2
-    // 3 4 5
-    // 6 7 8
-    g.addEdge(0,1,2);
-    g.addEdge(0,3,2);
-    g.addEdge(1,2,2);
-    g.addEdge(1,4,2);
-    g.addEdge(2,5,2);
-    g.addEdge(3,4,2);
-    g.addEdge(4,5,2);
-    g.addEdge(3,6,2);
-    g.addEdge(4,7,2);
-    g.addEdge(6,7,2);
-    g.addEdge(5,8,2);
-    g.addEdge(7,8,2);
+    // creating a graph with 25 vertices (5*5)
+    // 0  1  2  3  4
+    // 5  6  7  8  9
+    // 10 11 12 13 14
+    // 15 16 17 18 19
+    // 20 21 22 23 24
+    g.addEdge(0, 1, 2);
+    g.addEdge(0, 5, 2);
+    g.addEdge(1, 2, 2);
+    g.addEdge(1, 6, 2);
+    g.addEdge(2, 3, 2);
+    g.addEdge(2, 7, 2);
+    g.addEdge(3, 4, 2);
+    g.addEdge(3, 8, 2);
+    g.addEdge(4, 9, 2);
+    g.addEdge(5, 6, 2);
+    g.addEdge(5, 10, 2);
+    g.addEdge(6, 7, 2);
+    g.addEdge(6, 11, 2);
+    g.addEdge(7, 8, 2);
+    g.addEdge(7, 12, 2);
+    g.addEdge(8, 9, 2);
+    g.addEdge(8, 13, 2);
+    g.addEdge(9, 14, 2);
+    g.addEdge(10, 11, 2);
+    g.addEdge(10, 15, 2);
+    g.addEdge(11, 12, 2);
+    g.addEdge(11, 16, 2);
+    g.addEdge(12, 13, 2);
+    g.addEdge(12, 17, 2);
+    g.addEdge(13, 14, 2);
+    g.addEdge(13, 18, 2);
+    g.addEdge(14, 19, 2);
+    g.addEdge(15, 16, 2);
+    g.addEdge(15, 20, 2);
+    g.addEdge(16, 17, 2);
+    g.addEdge(16, 21, 2);
+    g.addEdge(17, 18, 2);
+    g.addEdge(17, 22, 2);
+    g.addEdge(18, 19, 2);
+    g.addEdge(18, 23, 2);
+    g.addEdge(19, 24, 2);
+    g.addEdge(20, 21, 2);
+    g.addEdge(21, 22, 2);
+    g.addEdge(22, 23, 2);
+    g.addEdge(23, 24, 2);
+  
 }
 
 
--- i222733_F_FinalProject/main.cpp
+++ i222733_F_FinalProject/main.cpp
@@ -6,11 +6,103 @@
 #include <cstdlib>
 #include <ctime>
 #include "Header.h"
+class Car{
+    char symbol;
+public:
+    Car():symbol('C'){}
+    Car(char s):symbol(s){}
+    char getSymbol(){return symbol;}
+    void setSymbol(char s){symbol = s;}
+    void moveCar(Graph&g, char direction,int& carPlace,int rows,int cols) {
+        system("clear");
+        if(direction=='a') {
+            if(carPlace%cols == 0) {
+                cout<<"Can't move left"<<endl;
+            }
+            else {
+                if(g.doesEdgeExist(carPlace, carPlace-1)) {
+                    carPlace--;
+                }
+                else {
+                    cout<<"Can't move left"<<endl;
+                }
+            }
+        }
+        else if(direction == 'w') {
+            if(carPlace-cols < 0) {
+                cout<<"Can't move up"<<endl;
+            }
+            else {
+                if(g.doesEdgeExist(carPlace, carPlace-cols)) {
+                    carPlace-=cols;
+                }
+                else {
+                    cout<<"Can't move up"<<endl;
+                }
+            }
+        }
+        else if(direction == 's') {
+            if(carPlace+cols >= rows*cols) {
+                cout<<"Can't move down"<<endl;
+            }
+            else {
+                if(g.doesEdgeExist(carPlace, carPlace+cols)) {
+                    carPlace+=cols;
+                }
+                else {
+                    cout<<"Can't move down"<<endl;
+                }
+            }
+        }
+        else if(direction == 'd') {
+            if(carPlace%cols == cols-1) {
+                cout<<"Can't move right"<<endl;
+            }
+            else {
+                if(g.doesEdgeExist(carPlace, carPlace+1)) {
+                    carPlace++;
+                }
+                else {
+                    cout<<"Can't move right"<<endl;
+                }
+            }
+        }
+    }
+};
 int main() {
-    Graph g(9);
+    Car car;
+    Graph g(25);
     generateMap(g);
-    for(int i=0;i<9;i++){
-        g.display(3,3,i);
+    // when a,w,s,d is pressed, the car moves in that direction
+    char c;
+    int rows=5, columns=5;
+    int carPlace = 0;
+    g.display(rows,columns,carPlace);
+    cout<<"Press a,w,s,d to move the car\nPress q to quit"<<endl;
+    while(true) {
+        cin>>c;
+        if(c == 'a') {
+            car.moveCar(g, 'a',carPlace,rows,columns);
+            g.display(rows,columns,carPlace);
+        }
+        else if(c == 'w') {
+            car.moveCar(g, 'w',carPlace,rows,columns);
+            g.display(rows,columns,carPlace);
+        }
+        else if(c == 's') {
+            car.moveCar(g, 's',carPlace,rows,columns);
+            g.display(rows,columns,carPlace);
+        }
+        else if(c == 'd') {
+            car.moveCar(g, 'd',carPlace,rows,columns);
+            g.display(rows,columns,carPlace);
+        }
+        else if(c == 'q') {
+            break;
+        }
+        else {
+            cout<<"Invalid input"<<endl;
+        }
     }
     return 0;
 }
